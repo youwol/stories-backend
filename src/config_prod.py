31c8f00be2a2e44d1b76d8b1c04d8b355265ade8
Julien DÉCHARNE <jdecharne@youwol.com>
2022-07-25 17:41:23 +0200

    🏗️ build docker image from submodule py-youwol

--- src/config_prod.py
+++ src/config_prod.py
@@ -1,25 +1,34 @@
 import os
 
-from config_common import on_before_startup, cache_prefix
-
+from config_common import on_before_startup
 from youwol_stories_backend import Configuration, Constants
-
-from youwol_utils import StorageClient, DocDbClient, AuthClient, CacheClient, get_headers_auth_admin_from_env
+from youwol_utils import StorageClient, DocDbClient, get_authorization_header
 from youwol_utils.clients.assets_gateway.assets_gateway import AssetsGatewayClient
+from youwol_utils.clients.oidc.oidc_config import PrivateClient, OidcInfos
 from youwol_utils.context import DeployedContextReporter
 from youwol_utils.http_clients.stories_backend import STORIES_TABLE, DOCUMENTS_TABLE, DOCUMENTS_TABLE_BY_ID
-from youwol_utils.middlewares import Middleware
+from youwol_utils.middlewares import AuthMiddleware
 from youwol_utils.servers.fast_api import FastApiMiddleware, ServerOptions, AppConfiguration
 
 
 async def get_configuration():
-    required_env_vars = ["AUTH_HOST", "AUTH_CLIENT_ID", "AUTH_CLIENT_SECRET", "AUTH_CLIENT_SCOPE"]
+    required_env_vars = [
+        "OPENID_BASE_URL",
+        "OPENID_CLIENT_ID",
+        "OPENID_CLIENT_SECRET",
+    ]
 
     not_founds = [v for v in required_env_vars if not os.getenv(v)]
     if not_founds:
         raise RuntimeError(f"Missing environments variable: {not_founds}")
 
-    openid_host = os.getenv("AUTH_HOST")
+    openid_infos = OidcInfos(
+        base_uri=os.getenv("OPENID_BASE_URL"),
+        client=PrivateClient(
+            client_id=os.getenv("OPENID_CLIENT_ID"),
+            client_secret=os.getenv("OPENID_CLIENT_SECRET")
+        )
+    )
 
     async def _on_before_startup():
         await on_before_startup(service_config)
@@ -43,7 +52,7 @@ async def get_configuration():
             replication_factor=2
         ),
         assets_gtw_client=AssetsGatewayClient(url_base="http://assets-gateway"),
-        admin_headers=await get_headers_auth_admin_from_env()
+        admin_headers=await get_authorization_header(openid_infos)
     )
     server_options = ServerOptions(
         root_path='/api/stories-backend',
@@ -51,11 +60,10 @@ async def get_configuration():
         base_path="",
         middlewares=[
             FastApiMiddleware(
-                Middleware, {
-                    "auth_client": AuthClient(url_base=f"https://{openid_host}/auth"),
-                    "cache_client": CacheClient(host="redis-master.infra.svc.cluster.local", prefix=cache_prefix),
-                    # healthz need to not be protected as it is used for liveness prob
-                    "unprotected_paths": lambda url: url.path.split("/")[-1] == "healthz"
+                AuthMiddleware, {
+                    'openid_infos': openid_infos,
+                    'predicate_public_path': lambda url:
+                    url.path.endswith("/healthz")
                 }
             )
         ],
